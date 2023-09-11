cf2ac6af82e09f51214945785c24d279ff33126d
backm <yousseffmohamedd22@gmail.com>
2023-09-11 16:18:55 +0300

    fix count

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -13,9 +13,28 @@ const AppError = require("../utils/appError");
 
 exports.getAllProducts = catchAsync(async (req, res, next) => {
   // #swagger.tags = ['Product']
+  /*  #swagger.description = 'TO CUSTOMIZE YOUR REQUEST: ?price[gte]=1000&price[lte]=5000 OR ?category[in]=electronics,clothing OR ?page=3&sort=-createdAt&limit=20&fields=name,description ' */
+  /*  #swagger.parameters['limit'] = {
+              in: 'query',
+              description: 'Page size: ex: ?limit=10',
+type: 'number'
+      } */
+  /*  #swagger.parameters['fields'] = {
+              in: 'query',
+              description: 'example: ?fields=name,description' ,
+      } */
+  /*  #swagger.parameters['page'] = {
+              in: 'query',
+              description: 'indexing page: ex: ?page=2',
+type: 'number'
+      } */
+  /*  #swagger.parameters['sort'] = {
+              in: 'query',
+              description: 'example: ?sort=name,-createdAt',
+      } */
   let filter = {};
   if (req.params.id) filter = { model: req.params.id };
-  const count = await Model.find()
+  const count = await Product.find()
   const features = new APIFeatures(Product.find(filter), req.query)
     .filter()
     .sort()
